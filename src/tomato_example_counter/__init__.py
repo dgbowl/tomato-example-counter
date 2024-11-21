5a4327fb994a4b70ad514a7b1fb52f77f5751436
Peter Kraus <peter.kraus@tu-berlin.de>
2024-11-21 08:02:25 +0100

    Fix `random` technique.

--- src/tomato_example_counter/__init__.py
+++ src/tomato_example_counter/__init__.py
@@ -16,6 +16,11 @@ class DriverInterface(ModelInterface):
         _min: float
         _val: float
 
+        def __init__(self, driver, key, **kwargs):
+            super().__init__(driver, key, **kwargs)
+            self._min = 0.0
+            self._max = 1.0
+
         def do_task(
             self, task: Task, t_start: float, t_now: float, **kwargs: dict
         ) -> None:
@@ -29,9 +34,9 @@ class DriverInterface(ModelInterface):
 
         def set_attr(self, attr: str, val: Any, **kwargs: dict) -> None:
             if attr == "max":
-                self._max = val if val is not None else 1.0
+                self._max = val
             elif attr == "min":
-                self._min = val if val is not None else 0.0
+                self._min = val
 
         def get_attr(self, attr: str, **kwargs: dict) -> Any:
             if hasattr(self, f"_{attr}"):
